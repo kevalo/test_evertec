8735a728edf1f0c17b84dd7b60c8095c81cd2cda
Kevin vargas <vargaskevin19@gmail.com>
2022-10-07 19:50:06 -0500

    Breeze login and migrations

---
+++ database/migrations/2022_10_08_000002_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', static function (Blueprint $table) {
+            $table->id();
+            $table->string('status', 20);
+            $table->timestamps();
+            $table->foreignId('customer_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
